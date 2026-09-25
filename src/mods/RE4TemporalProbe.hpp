56c67ee83ffd13523320097a669d2eeb4a202a32
onehoon <142466759+onehoon@users.noreply.github.com>
2026-09-26 01:55:46 +0900

    Track RE4 scene anchors for post effects

--- src/mods/RE4TemporalProbe.hpp
+++ src/mods/RE4TemporalProbe.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <array>
 #include <atomic>
+#include <cstdint>
 
 #include <sdk/Renderer.hpp>
 
@@ -13,10 +15,17 @@ public:
 
     void on_draw_ui() override;
     void on_scene_layer_draw(sdk::renderer::layer::Scene* layer, void* render_context) override;
+    void on_post_effect_layer_draw(sdk::renderer::layer::PostEffect* layer, void* render_context) override;
 
 private:
     std::atomic<bool> m_enabled{false};
     std::atomic<int> m_scenario{0};
     std::atomic<uint32_t> m_non_primary_scene_callbacks{0};
+
     re4_temporal_probe::SampleBudget m_sample_budget;
+    re4_temporal_probe::SampleBudget m_post_effect_sample_budget;
+
+    std::atomic<uintptr_t> m_last_depth_resource{0};
+    std::atomic<uintptr_t> m_last_velocity_resource{0};
+    std::array<std::atomic<uintptr_t>, 4> m_last_scene_rtv_resources{};
 };
