d6dc6e6741753fb17095fac3ea571429e09df184
onehoon <142466759+onehoon@users.noreply.github.com>
2026-09-26 08:41:42 +0900

    Test RE4 overlay boundary probe gating

--- tests/RE4TemporalProbeTests.cpp
+++ tests/RE4TemporalProbeTests.cpp
@@ -8,6 +8,7 @@ int main() {
     using namespace re4_temporal_probe;
     int scene{};
     int post_effect{};
+    int overlay{};
     int render_context{};
 
     CHECK(!should_process_scene(false, true, &scene));
@@ -21,6 +22,12 @@ int main() {
     CHECK(!should_process_post_effect(true, true, &post_effect, nullptr));
     CHECK(should_process_post_effect(true, true, &post_effect, &render_context));
 
+    CHECK(!should_process_overlay(false, true, &overlay, &render_context));
+    CHECK(!should_process_overlay(true, false, &overlay, &render_context));
+    CHECK(!should_process_overlay(true, true, nullptr, &render_context));
+    CHECK(!should_process_overlay(true, true, &overlay, nullptr));
+    CHECK(should_process_overlay(true, true, &overlay, &render_context));
+
     CHECK(!is_primary_scene(false, true, true));
     CHECK(!is_primary_scene(true, false, true));
     CHECK(!is_primary_scene(true, true, false));
@@ -29,6 +36,7 @@ int main() {
     CHECK(MAX_SAMPLES == 10);
     CHECK(MAX_SCENE_RTVS == 8);
     CHECK(MAX_POST_EFFECT_RTVS == 8);
+    CHECK(MAX_OVERLAY_RTVS == 8);
 
     SampleBudget interleaved_budget;
     uint32_t interleaved_captures{};
