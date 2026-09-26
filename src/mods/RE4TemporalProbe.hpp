9c2358f90ac3d465f1f55ecd9060929b20935ef9
onehoon <142466759+onehoon@users.noreply.github.com>
2026-09-26 10:05:41 +0900

    Pair RE4 Overlay pre and post boundary samples

--- src/mods/RE4TemporalProbe.hpp
+++ src/mods/RE4TemporalProbe.hpp
@@ -16,14 +16,13 @@ public:
     void on_draw_ui() override;
     void on_scene_layer_draw(sdk::renderer::layer::Scene* layer, void* render_context) override;
     void on_post_effect_layer_draw(sdk::renderer::layer::PostEffect* layer, void* render_context) override;
+    bool on_pre_overlay_layer_draw(sdk::renderer::layer::Overlay* layer, void* render_context) override;
     void on_overlay_layer_draw(sdk::renderer::layer::Overlay* layer, void* render_context) override;
-    void on_application_entry(void* entry, const char* name, size_t hash) override;
 
 private:
     std::atomic<bool> m_enabled{false};
     std::atomic<int> m_scenario{0};
     std::atomic<uint32_t> m_non_primary_scene_callbacks{0};
-    std::atomic<uint32_t> m_end_rendering_samples{0};
 
     re4_temporal_probe::SampleBudget m_sample_budget;
     re4_temporal_probe::SampleBudget m_post_effect_sample_budget;
@@ -32,16 +31,13 @@ private:
     std::atomic<uintptr_t> m_last_depth_resource{0};
     std::atomic<uintptr_t> m_last_velocity_resource{0};
     std::array<std::atomic<uintptr_t>, 4> m_last_scene_rtv_resources{};
-    std::atomic<uintptr_t> m_last_scene_layer{0};
-    std::atomic<uintptr_t> m_last_post_effect_layer{0};
-    std::atomic<uintptr_t> m_last_post_effect_target_state{0};
-    std::atomic<uintptr_t> m_last_post_effect_texture{0};
     std::atomic<uintptr_t> m_last_post_effect_resource{0};
     std::atomic<uint32_t> m_last_post_effect_frame{0};
 
-    std::atomic<uintptr_t> m_last_overlay_layer{0};
-    std::atomic<uintptr_t> m_last_overlay_target_state{0};
-    std::atomic<uintptr_t> m_last_overlay_texture{0};
-    std::atomic<uintptr_t> m_last_overlay_resource{0};
-    std::atomic<uint32_t> m_last_overlay_frame{0};
+    std::atomic<uint32_t> m_overlay_boundary_sample{0};
+    std::atomic<uint32_t> m_overlay_pre_frame{0};
+    std::atomic<uintptr_t> m_overlay_pre_current_target{0};
+    std::atomic<uintptr_t> m_overlay_pre_current_resource{0};
+    std::atomic<uintptr_t> m_overlay_pre_main_target{0};
+    std::atomic<uintptr_t> m_overlay_pre_main_resource{0};
 };
